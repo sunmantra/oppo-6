3b1cfda57f186e39fd2ee3d019dcd7d4b3c60381
sunmantra <zcernyh14@gmail.com>
2020-11-21 08:39:02 +0700

    реализация

--- mine.cpp
+++ mine.cpp
@@ -1,5 +1,20 @@
+#include <iostream>
+using namespace std;
+
 double my_pow(double a, unsigned int n){
     for(int i=0; i<n;i++){
         a*=a;
     }
+
+}
+
+int mine(){
+     setlocale(LC_ALL, "rus");
+
+    double a;
+    int n;
+
+    cout<<"введи число и степень\n";
+    cin>> a>>n;
+    my_pow(a, n);
 }
